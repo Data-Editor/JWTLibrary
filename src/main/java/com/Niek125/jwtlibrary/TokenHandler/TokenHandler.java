e6b030d0406ab3d7bd43e652172d7053eb6861a5
Niek125 <niek.sleddens@gmail.com>
2019-10-30 09:56:55 +0100

    debug for travis

--- src/main/java/com/Niek125/jwtlibrary/TokenHandler/TokenHandler.java
+++ src/main/java/com/Niek125/jwtlibrary/TokenHandler/TokenHandler.java
@@ -25,18 +25,21 @@ public class TokenHandler<T> {
             if (!JsonPath.parse(token.getHeader()).read("$.typ").equals("JWT")) {
                 return TokenValidationResponse.NO_JWT;
             }
+            System.out.println(TokenValidationResponse.NO_JWT);
             if (Long.parseLong(JsonPath.parse(token.getPayload()).read("$.exp").toString()) - maxDelay < System.currentTimeMillis()) {
                 return TokenValidationResponse.EXPIRED;
             }
+            System.out.println(TokenValidationResponse.EXPIRED);
             if (blackList.isBlacklisted(JsonPath.parse(token.getPayload()).read("$.jti"))) {
                 return TokenValidationResponse.BLACKLISTED;
             }
+            System.out.println(TokenValidationResponse.BLACKLISTED);
             if (sigRep.isForged(token, JsonPath.parse(token.getHeader()).read("$.alg"), JsonPath.parse(token.getPayload()).read("$.exp"))) {
                 return TokenValidationResponse.FORGED;
             }
+            System.out.println(TokenValidationResponse.FORGED);
             authMaker.makeAuthObject(token.getPayload());
         } catch (Exception e) {//not the right format
-            System.out.println(e.toString());
             return TokenValidationResponse.FORGED;
         }
         return TokenValidationResponse.GOOD;
